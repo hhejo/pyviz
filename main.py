8fe10886482d4fcbe57d6a9e167003195f3a2a10
hhejo <litsmainjof@gmail.com>
2024-10-10 15:50:15 +0900

    feat: add chatgpt api

--- main.py
+++ main.py
@@ -5,14 +5,15 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from konlpy.tag import Okt
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+from matplotlib import rc
+from openai import OpenAI
 from dotenv import load_dotenv
 import ssl
 import time
 import os
-from collections import Counter
-from wordcloud import WordCloud
-import matplotlib.pyplot as plt
-from matplotlib import rc
+from collections import Counter, defaultdict
 # 
 import urllib.request
 from bs4 import BeautifulSoup
@@ -36,15 +37,19 @@ driver.get(base_url)
 driver.implicitly_wait(10)
 login_id = driver.find_element(By.CSS_SELECTOR, 'input[name="username"]')
 login_id.send_keys(os.environ.get('ID'))
+print('ID entered')
 login_pwd = driver.find_element(By.CSS_SELECTOR, 'input[name="password"]')
 login_pwd.send_keys(os.environ.get('PW'))
+print('PW entered')
 login_id.send_keys(Keys.ENTER)
+print('ENTER entered')
 time.sleep(5)
 
 # 게시물 목록
 food_url = 'https://www.instagram.com/hoseofood/'
 driver.get(food_url)
 driver.implicitly_wait(10)
+print('Connecting to https://www.instagram.com/hoseofood/')
 
 texts = []
 
@@ -54,20 +59,23 @@ article.click()
 driver.implicitly_wait(10)
 detail = driver.find_element(By.CSS_SELECTOR, '._a9zs h1')
 texts.append(detail.text)
+print(f'#001 post saved')
 
 # 계속 다음 게시글 가져오기
-COUNTS = 20
-for _ in range(COUNTS):
+COUNTS = 9
+for i in range(2, COUNTS + 1):
   next_button = driver.find_element(By.CSS_SELECTOR, '._aaqg ._abl-')
   next_button.click()
   time.sleep(0.3)
   detail = driver.find_element(By.CSS_SELECTOR, '._a9zs h1')
   texts.append(detail.text)
+  order = str(i)
+  print(f'#{order.rjust(3, '0')} post saved')
 
-# 명사 추출
-okt = Okt()
-tokens = okt.nouns(''.join(texts))
-print(tokens)
+# # 명사 추출
+# okt = Okt()
+# print(texts)
+# tokens = okt.nouns(''.join(texts))
 
 # # 빈도수 확인
 # LENGTH = 30
@@ -75,21 +83,55 @@ print(tokens)
 # top_nouns = dict(nouns_counter.most_common(LENGTH))
 # print(top_nouns)
 
-# 시각화
-font_path = '/Library/Fonts/AppleGothic'
-word = ' '.join(tokens)
-# max_words=100
-# stopwords=STOPWORDS
-wordcloud = WordCloud(font_path, background_color='white', colormap='Accent_r', width=800, height=800)
-wordcloud = wordcloud.generate(word)
-# wordcloud_words = wordcloud.generate_from_frequencies(word)
-array = wordcloud.to_array()
-fig = plt.figure(figsize=(10, 10))
-plt.imshow(array, interpolation='bilinear')
-plt.axis('off')
-plt.show()
-fig.savefig('wordcloud.png')
+# # 시각화
+# font_path = '/Library/Fonts/AppleGothic'
+# words = ' '.join(tokens)
+# # max_words=100
+# # stopwords=STOPWORDS
+# wordcloud = WordCloud(font_path, background_color='white', colormap='Accent_r', width=800, height=800)
+# wordcloud = wordcloud.generate(words)
+# # wordcloud_words = wordcloud.generate_from_frequencies(word)
+# array = wordcloud.to_array()
+# fig = plt.figure(figsize=(10, 10))
+# plt.imshow(array, interpolation='bilinear')
+# plt.axis('off')
+# plt.show()
+# fig.savefig('wordcloud.png')
 
 # 제외 단어 설정
 # from wordcloud import STOPWORDS
 # STOPWORDS.add('가산', '맛', '메뉴')
+
+# 음식 이름 추출
+# print('\n\n'.join(texts))
+# msg = '''
+# 오늘의 메뉴입니다. 선선한 날씨에 푹 끓인 따끈한 황태뭇국 어떠세요? 가을 무와 황태채에서 나온 육수의 감칠맛으로 구수하고 맛있는 황태뭇국과,
+
+# 질 좋은 돼지고기를 잘 익은 김치와 함께 볶아 더욱 맛있는 한국인의 주찬 돼지김치두루치기,
+
+# 바삭하게 튀겨낸 프라임치킨너겟 & 달콤한 스윗칠리,
+
+# 철판에서 노릇하게 부친 메밀전병에 알싸한 맛의 파채를 올려 포인트를 준 파채메밀전병,
+
+# 매콤한 소스에 아삭한 콩나물과 채소가 듬뿍, 쫄면의 쫄깃한 식감이 만나 많은 분들께서 좋아하시는 쫄면,
+
+# 부추와 갖은 채소를 송송 다져 듬뿍 넣은 조림간장에 간이 잘 배어 자꾸자꾸 손이 가는 마약계란장조림과
+
+# 얼갈이와 부추를 겉절이 양념장에 정성스레 버무려 맛을 더한 얼갈이겉절이가 오늘의 메뉴입니다❤
+
+# #가산 #가산디지털단지구내식당 #독산역구내식당 #가산디지털단지맛집 #가산맛집추천 #가디점심 #맛집그램 #맛스타그램 #먹스타그램 #오늘밥상 #한끼 #회사밥 #구내식당 #구내식당그램 #구내식당맛집 #호서구내식당 #식판 #식판샷 #우주최강가성비
+# '''
+msg = ''.join(texts)
+msg = f'{msg}\n위에서 음식이름을 뽑아줘 줄마다 숫자, 대시 이런 거 쓰지 말고 한 줄에 음식이름 하나씩만 미사여구 붙이지 말고'
+OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
+client = OpenAI(api_key=OPENAI_API_KEY)
+completion = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "user", "content": msg}
+    ]
+)
+print(completion.choices[0].message.content)
+
+li = completion.choices[0].message.content.split('\n')
+dict = defaultdict(int)
