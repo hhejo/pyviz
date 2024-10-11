c02a69132dcb00c3f7d41e542f6a88d97d6ef6b2
hhejo <litsmainjof@gmail.com>
2024-10-11 11:12:34 +0900

    feat: update main

--- main.py
+++ main.py
@@ -4,20 +4,25 @@ from controller.mbti_controller import  get_mbti_counts
 from controller.plot_controller import create_plot, create_wordcloud
 from controller.file_controller import load_csv, save_csv, save_txt
 
-file_path = os.path.join('assets', 'mbti_counts.csv')
-mbti_counts = {}
-if os.path.isfile(file_path):
-  print('저장된 데이터 사용')
-  mbti_counts = load_csv('assets/mbti_counts.csv')
-else:
-  scroll_counts = 500
-  start_sec, end_sec = 2, 3.5
-  titles, contents = crawl_texts(scroll_counts, start_sec, end_sec)
-  mbti_counts = get_mbti_counts(titles, contents)
-  create_plot('assets/mbti_plot.png', mbti_counts)
-  create_wordcloud('assets/mbti_wordcloud.png', mbti_counts)
-  save_csv('assets/mbti_counts.csv', mbti_counts)
-  save_txt('assets/titles.txt', titles)
-  save_txt('assets/contents.txt', contents)
 
-print('MBTI 통계: ', mbti_counts)
+def exec_crawl(scroll_counts, min_wait_sec, max_wait_sec):
+    csv_file_path = os.path.join('assets', 'mbti_counts.csv')  # /assets/mbti_counts.csv 파일 위치
+    mbti_counts = {}  # MBTI별 언급량 {MBTI_타입: 개수}
+    if os.path.isfile(csv_file_path):
+        print('저장된 데이터 사용')
+        mbti_counts = load_csv('assets/mbti_counts.csv')  # 저장된 파일이 있으면 크롤링하지 않고 해당 csv 파일 사용
+    else:
+        titles, contents = crawl_texts(scroll_counts, min_wait_sec, max_wait_sec)  # 게시글 제목, 내용 크롤링 (스크롤 횟수, 최소 대기시간, 최대 대기시간)
+        mbti_counts = get_mbti_counts(titles, contents)  # 크롤링한 제목, 내용으로부터 MBTI 언급량 얻기
+        create_plot('assets/mbti_plot.png', mbti_counts)  # MBTI 언급량으로 막대 그래프 생성하고 저장
+        create_wordcloud('assets/mbti_wordcloud.png', mbti_counts)  # MBTI 언급량으로 워드 클라우드 생성하고 저장
+        save_csv('assets/mbti_counts.csv', mbti_counts)  # MBTI 언급량을 CSV 파일로 저장
+        save_txt('assets/titles.txt', titles)  # 제목 모음을 txt 파일로 저장
+        save_txt('assets/contents.txt', contents)  # 내용 모음을 txt 파일로 저장
+
+    print('MBTI 언급량: ', mbti_counts)
+    return
+
+
+if __name__ == '__main__':
+    exec_crawl(1, 2, 3.5)
