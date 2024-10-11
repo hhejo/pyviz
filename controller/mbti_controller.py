164d70a11bdb0c13da68faf46dad393105aa041c
hhejo <litsmainjof@gmail.com>
2024-10-11 11:12:17 +0900

    feat: update controller

--- controller/mbti_controller.py
+++ controller/mbti_controller.py
@@ -1,65 +1,43 @@
-def get_korean(text):
-  if '엔프제' in text:
-    return 'ENFJ'
-  elif '엔프피' in text:
-    return 'ENFP'
-  elif '엔티제' in text:
-    return 'ENTJ'
-  elif '엔팁' in text:
-    return 'ENTP'
-  elif '엣프제' in text:
-    return 'ESFJ'
-  elif '엣프피' in text:
-    return 'ESFP'
-  elif '엣티제' in text:
-    return 'ESTJ'
-  elif '엣팁' in text:
-    return 'ESTP'
-  # 
-  elif '인프제' in text:
-    return 'INFJ'
-  elif '인프피' in text or '씹프피' in text:
-    return 'INFP'
-  elif '인티제' in text:
-    return 'INTJ'
-  elif '인팁' in text:
-    return 'INTP'
-  elif '잇프제' in text:
-    return 'ISFJ'
-  elif '잇프피' in text:
-    return 'ISFP'
-  elif '잇티제' in text:
-    return 'ISTJ'
-  elif '잇팁' in text:
-    return 'ISTP'
-  else:
-    return ''
+def get_mbti_type_from_korean(text):
+    mbti_korean_types_e = {'엔프제': 'ENFJ', '엔프피': 'ENFP', '엔티제': 'ENTJ', '엔팁': 'ENTP', '엣프제': 'ESFJ', '엣프피': 'ESFP', '엣티제': 'ESTJ', '엣팁': 'ESTP'}
+    mbti_korean_types_i = {'인프제': 'INFJ', '인프피': 'INFP', '인티제': 'INTJ', '인팁': 'INTP', '잇프제': 'ISFJ', '잇프피': 'ISFP', '잇티제': 'ISTJ', '잇팁': 'ISTP', '씹프피': 'INFP'}
+
+    for ko, en in mbti_korean_types_e.items():
+        if ko in text:
+            return en, text.count(ko)
+
+    for ko, en in mbti_korean_types_i.items():
+        if ko in text:
+            return en, text.count(ko)
+
+    return '', 0
 
 
 def get_mbti_counts(titles, contents):
-  mbtis = []
-  for a in 'EI':
-    for b in 'NS':
-      for c in 'FT':
-        for d in 'JP':
-          mbtis.append(a + b + c + d)
-  mbti_counts = {key: 0 for key in mbtis}
-  print('제목에서 MBTI 추출 시작')
-  for title in titles:
-    for mbti in mbtis:
-      if mbti in title:
-        mbti_counts[mbti] += 1
-    result = get_korean(title)
-    if result:
-      mbti_counts[result] += 1
-  print('제목에서 MBTI 추출 완료')
-  print('내용에서 MBTI 추출 시작')
-  for content in contents:
-    for mbti in mbtis:
-      if mbti in content:
-        mbti_counts[mbti] += 1
-    result = get_korean(content)
-    if result:
-      mbti_counts[result] += 1
-  print('내용에서 MBTI 추출 완료')
-  return mbti_counts
+    mbti_types = []  # MBTI 타입 16개 생성
+
+    for a in 'EI':
+        for b in 'NS':
+            for c in 'FT':
+                for d in 'JP':
+                    mbti_types.append(a + b + c + d)
+
+    mbti_counts = {key: 0 for key in mbti_types}  # {MBTI_타입: 개수}
+
+    for title in titles:
+        for mbti_type in mbti_types:
+            if mbti_type in title:
+                mbti_counts[mbti_type] += title.count(mbti_type)
+        mbti_type_from_korean, counts = get_mbti_type_from_korean(title)
+        if mbti_type_from_korean:
+            mbti_counts[mbti_type_from_korean] += counts
+
+    for content in contents:
+        for mbti_type in mbti_types:
+            if mbti_type in content:
+                mbti_counts[mbti_type] += content.count(mbti_type)
+        mbti_type_from_korean, counts = get_mbti_type_from_korean(content)
+        if mbti_type_from_korean:
+            mbti_counts[mbti_type_from_korean] += counts
+
+    return mbti_counts
